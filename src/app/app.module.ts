fc69948982717843d5145bf7066c77ae9b25afe4
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-09-11 16:19:02 -0300

    add listagem de cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListarNotasComponent } from './components/notas/listar-notas/listar-notas.component';
+import { CardNotaComponent } from './components/notas/card-nota/card-nota.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ListarNotasComponent,
+    CardNotaComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
