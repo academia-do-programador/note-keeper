50281528b9c9b88ae78a4fb026a4e179e1cca137
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-09-12 14:42:20 -0300

    add criar-nota component template

--- src/app/components/notas/criar-nota/criar-nota.component.ts
+++ src/app/components/notas/criar-nota/criar-nota.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Nota } from '../nota';
 
 @Component({
   selector: 'app-criar-nota',
   templateUrl: './criar-nota.component.html',
   styleUrls: ['./criar-nota.component.css'],
 })
-export class CriarNotaComponent {}
+export class CriarNotaComponent {
+  nota: Nota;
+
+  constructor() {
+    this.nota = new Nota(
+      'Lavar o cachorro 🦮',
+      'Pegar a toalha > pegar o shampoo',
+      'dark',
+      0
+    );
+  }
+
+  criarNota() {
+    alert(this.nota.titulo);
+  }
+}
--- src/app/components/notas/nota.ts
+++ src/app/components/notas/nota.ts
@@ -1,6 +1,15 @@
-export interface Nota {
+export class Nota {
   id?: number;
   titulo: string;
   conteudo: string;
-  tema: string;
+  tema: Tema;
+
+  constructor(titulo: string, conteudo: string, tema: Tema, id?: number) {
+    this.id = id;
+    this.titulo = titulo;
+    this.conteudo = conteudo;
+    this.tema = tema;
+  }
 }
+
+type Tema = 'info' | 'warning' | 'danger' | 'dark';
