df2abb5e672888ff8ea6959d5f3205ac1f2b84df
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-09-12 14:16:16 -0300

    add criar-nota template

---
+++ src/app/components/notas/criar-nota/criar-nota.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-nota',
+  templateUrl: './criar-nota.component.html',
+  styleUrls: ['./criar-nota.component.css'],
+})
+export class CriarNotaComponent {}
