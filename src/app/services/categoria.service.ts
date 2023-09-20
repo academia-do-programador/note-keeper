c06e54f39115886ad5ee722d888d58c8b2fa4f73
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-09-20 16:02:29 -0300

    add: environment variables

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Categoria } from '../models/categoria';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoriaService {
-  private API_URL = 'http://localhost:3000/categorias';
+  private API_URL = `${environment.API_URL}/api/categorias`;
 
   constructor(private http: HttpClient) {}
 
